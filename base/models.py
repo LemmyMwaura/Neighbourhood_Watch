eab44bbd690f00f210b001c4184d1c476b9f0a11
Lemmy Mwaura <lemminton@gmail.com>
2022-04-15 12:05:20 +0300

    setup and registered models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,86 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['updated', 'created']
+
+    def __str__(self):
+        return self.name
+
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.delete()
+  
+    def update_neighbourhood():
+        pass
+
+    def update_occupants():
+        pass
+
+    @classmethod
+    def find_neighbourhood(cls,neigborhood_id):
+        return cls.objects.filter(id=neigborhood_id)
+
+
+class Profile(models.Model):
+    user_profile = models.OneToOneField(User, on_delete=models.CASCADE)
+    users_neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.DO_NOTHING, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['updated', 'created']
+
+    def __str__(self):
+        return self.user_profile.username
+
+class Post(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    message = models.TextField()
+    # comments = models.ForeignKey('Comment',related_name='comments', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['updated', 'created']
+
+    def __str__(self):
+        return self.title
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+class Business(models.Model):
+    name = models.CharField(max_length=150)
+    users_name = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    Neighbourhood_bsns = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['updated', 'created']
+
+    def __str__(self):
+        return f'{self.name} business'
+
+    def create_business(self):
+        self.save()
+  
+    def update_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls,business_id):
+        cls.objects.filter(id=business_id)
